f822546dda7d47f17f7ef8cdea8ff9b206e20e9d
Vladislav Zainullin <mittoface02@mail.ru>
2024-01-17 14:46:52 +0300

    Добавляет data source для страницы пользователя

---
+++ lib/data/remote_data_sources/profile/profile_data_source.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+
+import '../../../domain/models/list_model.dart';
+import '../../../domain/models/user_preview.dart';
+import '../../../data/remote_data_sources/models/list_model.dart'
+    as source_source_list_model;
+import '../../../data/remote_data_sources/models/user_preview.dart'
+    as source_user_preview;
+
+class ProfileDataSource {
+  final Dio dio;
+
+  ProfileDataSource(this.dio);
+
+  Future<ListModel<UserPreview>> getProfiles() async {
+    final result = await dio.get('/user');
+    final model = source_source_list_model.ListModel.fromJson(result.data)
+        .toEntity<UserPreview>(
+      (a) => source_user_preview.UserPreview.fromJson(a).toEntity(),
+    ) as ListModel<UserPreview>;
+
+    return model;
+  }
+
+  Future<UserPreview> updateProfile(
+      {required String profileId, required String name}) async {
+    final result = await dio.put('/user/$profileId', data: {
+      'firstName': name,
+    });
+
+    var userPreview =
+        source_user_preview.UserPreview.fromJson(result.data).toEntity();
+    return userPreview;
+  }
+
+  Future<UserPreview> updateProfileUserPhoto(
+      {required String profileId, required String userPicture}) async {
+    final result =
+        await dio.put('/user/$profileId', data: {'picture': userPicture});
+
+    var userPreview =
+        source_user_preview.UserPreview.fromJson(result.data).toEntity();
+    return userPreview;
+  }
+}
