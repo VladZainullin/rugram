4a61d76b2dbec7613297fd7dc21c1c163a8527ac
Vladislav Zainullin <mittoface02@mail.ru>
2024-01-19 00:16:54 +0300

    Удаляет неиспользуемый код

--- lib/features/profile/profile_page.dart
+++ lib/features/profile/profile_page.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rugram/features/profile/widgets/profile_widget.dart';
 import '../../data/remote_data_sources/profile/profile_data_source.dart';
-import '../../domain/models/user_preview.dart';
 import '../home/bloc/posts_cubit.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  _ProfilePageState createState() => _ProfilePageState();
+  State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage>
     with TickerProviderStateMixin {
+
   late final ProfileDataSource profileDataSource;
-  late TabController tabController;
+
   late final PostsCubit postsCubit;
-  TextEditingController nameController = TextEditingController();
-  String nickname = "...";
-  late UserPreview user;
+
+  late final String userId;
+  late final String userFullName;
+
+  late TextEditingController nameController;
+  late TextEditingController surnameController;
+
   List<String> imageUrls = [];
 
 
@@ -33,29 +36,9 @@ class _ProfilePageState extends State<ProfilePage>
           automaticallyImplyLeading: false,
           backgroundColor: Colors.transparent,
           title: Text(
-            nickname,
+            userFullName,
             style: const TextStyle(fontWeight: FontWeight.bold),
           ),
-          actions: const [
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.add_box_outlined,
-                    size: 30,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.table_rows_rounded,
-                    size: 30,
-                  ),
-                ),
-              ],
-            )
-          ],
         ),
         backgroundColor: Colors.transparent,
         body: CustomScrollView(
@@ -64,10 +47,24 @@ class _ProfilePageState extends State<ProfilePage>
             SliverPadding(
               padding: const EdgeInsets.symmetric(horizontal: 6.0),
               sliver: SliverToBoxAdapter(
-                child: ProfileWidget(
-                  imageUrls: imageUrls,
-                  nickname: nickname,
-                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: Text(
+                            userFullName,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
               ),
             ),
             SliverToBoxAdapter(
@@ -95,86 +92,38 @@ class _ProfilePageState extends State<ProfilePage>
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Container(
-                          height: 32,
-                          width: 32,
-                          decoration: BoxDecoration(
-                              color: Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(8)),
-                          child: const Center(
-                            child: Icon(
-                              Icons.person_add,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
+                      )
                     ],
                   ),
                 ],
               ),
             ),
             SliverToBoxAdapter(
-              child: TabBar(
-                controller: tabController,
-                tabs: const [
-                  Tab(icon: Icon(Icons.video_collection_outlined)),
-                  Tab(icon: Icon(Icons.person_add_alt_outlined)),
-                ],
-              ),
-            ),
-            SliverFillRemaining(
-              child: TabBarView(
-                controller: tabController,
-                children: [
-                  BlocBuilder<PostsCubit, PostsState>(
-                      bloc: postsCubit,
-                      builder: (context, state) {
-                        return switch (state) {
-                          PostsLoadedState() => GridView.builder(
-                              shrinkWrap: true,
-                              itemCount: state.postsInfo.data.length,
-                              gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 3,
-                                crossAxisSpacing: 3,
-                                mainAxisSpacing: 3,
-                                childAspectRatio: 1,
-                              ),
-                              itemBuilder: (context, index) {
-                                return Image.network(
-                                  state.postsInfo.data[index].image,
-                                  width: double.infinity,
-                                  height: double.infinity,
-                                  fit: BoxFit.cover,
-                                );
-                              }),
-                          _ => const Center(child: CircularProgressIndicator()),
-                        };
-                      }),
-                  const Center(
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 50,
-                        ),
-                        Icon(
-                          Icons.person_add,
-                          color: Colors.black,
-                          size: 60,
-                        ),
-                        Text(
-                          "Фотографии и видео с Вами",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+              child: BlocBuilder<PostsCubit, PostsState>(
+                  bloc: postsCubit,
+                  builder: (context, state) {
+                    return switch (state) {
+                      PostsLoadedState() => GridView.builder(
+                          shrinkWrap: true,
+                          itemCount: state.postsInfo.data.length,
+                          gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 3,
+                            mainAxisSpacing: 3,
+                            childAspectRatio: 1,
+                          ),
+                          itemBuilder: (context, index) {
+                            return Image.network(
+                              state.postsInfo.data[index].image,
+                              width: double.infinity,
+                              height: double.infinity,
+                              fit: BoxFit.cover,
+                            );
+                          }),
+                      _ => const Center(child: CircularProgressIndicator()),
+                    };
+                  })
             ),
           ],
         ),
@@ -185,23 +134,33 @@ class _ProfilePageState extends State<ProfilePage>
   @override
   void initState() {
     super.initState();
-    tabController = TabController(length: 2, vsync: this);
     postsCubit = PostsCubit(context.read())..initWithTag(tag: "person");
     profileDataSource = context.read<ProfileDataSource>();
     init();
-    // loadImages();
   }
 
   Future<void> init() async {
     final usersInfo = await profileDataSource.getProfiles();
-    user = usersInfo.data[10];
-    nickname = user.firstName;
+    final user = usersInfo.data[10];
+
+    userFullName = user.fullName;
+    userId = user.id;
+
+    nameController = TextEditingController(text: user.firstName);
+    surnameController = TextEditingController(text: user.lastName);
+
     setState(() {});
   }
 
-  Future<void> update({required String name}) async {
-    final updatedUser = await profileDataSource.updateProfile(profileId: user.id, name: name);
-    nickname = updatedUser.firstName;
+  Future<void> update({required String name, required String surname}) async {
+    final user = await profileDataSource.updateProfile(profileId: userId, name: name, surname: surname);
+
+    userFullName = user.fullName;
+    userId = user.id;
+
+    nameController = TextEditingController(text: user.firstName);
+    surnameController = TextEditingController(text: user.lastName);
+
     setState(() {});
   }
 
@@ -211,15 +170,21 @@ class _ProfilePageState extends State<ProfilePage>
       builder: (BuildContext context) {
         return AlertDialog(
           title: const Text('Редактирование профиля'),
-          content: TextFormField(
-            controller: nameController,
-            decoration: const InputDecoration(labelText: 'Новое имя'),
-          ),
+          content: Column(children: [
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'Введите имя'),
+            ),
+            TextFormField(
+              controller: surnameController,
+              decoration: const InputDecoration(labelText: 'Введите фамилию'),
+            ),
+          ]),
           actions: <Widget>[
             TextButton(
               onPressed: ()
               {
-                update(name: nameController.text);
+                update(name: nameController.text, surname: surnameController.text);
                 Navigator.pop(context);
               },
               child: const Text('Сохранить'),
